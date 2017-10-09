df285a899f29320258b8d15dd637165641ad2545
LJ Reader <lawrencereader@outlook.com>
2017-10-08 22:58:50 -0600

    India

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,84 +5,80 @@ var words = [
     ["E","D","U","C","A","T","I","O","N"],
     ["C","H","O","C","O","L","A","T","E"],
     ["S","A","R","L","A","C","C'"]
-]
+];
 var random = Math.floor((Math.random()*(words.length-1)));
 
-var rndmwrd = words[random]; // the word to guess will be chosen from the array above
-var ratewort = new Array(lsgwort.length);
-var fehler = 0;
+var randomWord = words[random]; // the word to guess will be chosen from the array above
+var guessField = new Array(randomWord.length);
+var error = 0;
 
 // every letter in the word is symbolized by an underscore in the guessfield
-for (var i = 0; i < ratewort.length; i++){
-    ratewort[i] = "_ ";
+for (var i = 0; i < guessField.length; i++){
+    guessField[i] = "_";
 }
 
 // prints the guessfield
-function printRatewort(){
-    for (var i = 0; i < ratewort.length; i++){
-        var ratefeld = document.getElementById("ratefeld");
-        var buchstabe = document.createTextNode(ratewort[i]);
-        ratefeld.appendChild(buchstabe);
+function printGuessField(){
+    for (var i = 0; i < guessField.length; i++){
+        var rateField = document.getElementById("rateField");
+        var letter = document.createTextNode(guessField[i]);
+        rateField.appendChild(letter);
     }
 }
 
 //checks if the the letter provided by the user matches one or more of the letters in the word
-var pruefeZeichen = function(){
-    var f = document.rateformular;
-    var b = f.elements["ratezeichen"];
-    var zeichen = b.value; // the letter provided by the user
-    zeichen = zeichen.toUpperCase();
-    for (var i = 0; i < lsgwort.length; i++){
-        if(lsgwort[i] === zeichen){
-            ratewort[i] = zeichen + " ";
-            var treffer = true;
+var checkMeCharacter = function(){
+    var f = document.rateFormula;
+    var b = f.elements["rateCharacter"];
+    var character = b.value; // the letter provided by the user
+    character = character.toUpperCase();
+    for (var i = 0; i < randomWord.length; i++){
+        if(randomWord[i] === character){
+            guessField[i] = character + " ";
+            var hit = true;
         }
         b.value = "";
     }
 
     //deletes the guessfield and replaces it with the new one
-    var ratefeld = document.getElementById("ratefeld");
-    ratefeld.innerHTML="";
-    printRatewort();
+    var rateField = document.getElementById("rateField");
+    rateField.innerHTML="";
+    printGuessField();
 
     // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
-    if(!treffer){
-        var gerateneBuchstaben = document.getElementById("gerateneBuchstaben");
-        var buchstabe = document.createTextNode(" " + zeichen);
-        gerateneBuchstaben.appendChild(buchstabe);
-        fehler++;
+    if(!hit){
+        var generateLetters = document.getElementById("generateLetters");
+        var letter = document.createTextNode(" " + character);
+        generateLetters.appendChild(letter);
+        error++;
         var hangman = document.getElementById("hangman");
-        hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + fehler + ".png";
+        hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + error + ".png";
     }
 
     //checks if all letters have been found
-    var fertig = true;
-    for (var i = 0; i < ratewort.length; i++){
-        if(ratewort[i] === "_ "){
-            fertig = false;
+    var finished = true;
+    for (var i = 0; i < guessField.length; i++){
+        if(guessField[i] === "_ "){
+            finished = false;
         }
     }
-    if(fertig){
+    if(finished){
         window.alert("May the Force Be With You!");
     }
 
-    // picture of han solo being pushed
-    if(fehler === 5){window.alert("Move them into position"}
-
 
 
     //Picture of JAba
     //once you got six wrong letters, you lose
-    if(fehler === 6){
+    if(error === 6){
         window.alert("Throw them into the pit!");
     }
-}
+};
 
 function init(){
-    printRatewort();
+    printGuessField();
 }
 
-window.onload = init;
 
 
 
@@ -99,94 +95,3 @@ window.onload = init;
 
 
 
-
-
-
-
-
-
-var lsgwoerter = [
-    ["C","H","E","W","B","A","C","C","A"],
-    ["T","A","T","O","O","I","N","E"],
-    ["W","E","B","D","E","S","I","G","N"],
-    ["E","D","U","C","A","T","I","O","N"],
-    ["C","H","O","C","O","L","A","T","E"],
-    ["G","E","R","M","A","N","Y"]
-]
-var random = Math.floor((Math.random()*(lsgwoerter.length-1)));
-
-var lsgwort = lsgwoerter[random]; // the word to guess will be chosen from the array above
-var ratewort = new Array(lsgwort.length);
-var fehler = 0;
-
-// every letter in the word is symbolized by an underscore in the guessfield
-for (var i = 0; i < ratewort.length; i++){
-    ratewort[i] = "_ ";
-}
-
-// prints the guessfield
-function printRatewort(){
-    for (var i = 0; i < ratewort.length; i++){
-        var ratefeld = document.getElementById("ratefeld");
-        var buchstabe = document.createTextNode(ratewort[i]);
-        ratefeld.appendChild(buchstabe);
-    }
-}
-
-//checks if the the letter provided by the user matches one or more of the letters in the word
-var pruefeZeichen = function(){
-    var f = document.rateformular;
-    var b = f.elements["ratezeichen"];
-    var zeichen = b.value; // the letter provided by the user
-    zeichen = zeichen.toUpperCase();
-    for (var i = 0; i < lsgwort.length; i++){
-        if(lsgwort[i] === zeichen){
-            ratewort[i] = zeichen + " ";
-            var treffer = true;
-        }
-        b.value = "";
-    }
-
-    //deletes the guessfield and replaces it with the new one
-    var ratefeld = document.getElementById("ratefeld");
-    ratefeld.innerHTML="";
-    printRatewort();
-
-    // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
-    if(!treffer){
-        var gerateneBuchstaben = document.getElementById("gerateneBuchstaben");
-        var buchstabe = document.createTextNode(" " + zeichen);
-        gerateneBuchstaben.appendChild(buchstabe);
-        fehler++;
-        var hangman = document.getElementById("hangman");
-        hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + fehler + ".png";
-    }
-
-    //checks if all letters have been found
-    var fertig = true;
-    for (var i = 0; i < ratewort.length; i++){
-        if(ratewort[i] === "_ "){
-            fertig = false;
-        }
-    }
-    if(fertig){
-        window.alert("You win!");
-    }
-
-    picture of han solo being pushed
-    if(fehler === 5){window.alert("Move them into position"}
-
-
-
-    //Picture of JAba
-    //once you got six wrong letters, you lose
-    if(fehler === 6){
-        window.alert("Throw them into the pit!");
-    }
-}
-
-function init(){
-    printRatewort();
-}
-
-window.onload = init;
